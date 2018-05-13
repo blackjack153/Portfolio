be560d8c9ccc96de8f8f4367d77f7442b983ce72
Hoang Long <hdl.uit@gmail.com>
2018-05-13 23:37:22 +0700

    pull 1

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { HeaderComponent } from './header/header.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    AboutComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
